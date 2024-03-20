a148a9195a5a618c097863ba5abe38c8d23ed82c
Rasydma <rasyidmubarok6@gmail.com>
2024-03-20 16:23:04 +0800

    uploand tugas pertemuan 5

---
+++ Pertemuan 5/data-kendaraan.php
@@ -0,0 +1,16 @@
+<?php
+    require_once 'class_kendaraan.php';
+
+    //object
+    $motor = new Motor('Motor', 'Besin', 2);
+    $submarine = new Submarine('Submarine', 'Solar', 1000);
+
+    //echo
+    echo "Info Motor:<br>";
+    $motor->getInfoMotor();
+    echo "<br>";
+
+    echo "Info Submarine:<br>";
+    $submarine->getInfoSubmarine();
+
+?>
